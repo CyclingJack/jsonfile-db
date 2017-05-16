be57552d544002dea5c9d05ea469c9e42780a93e
Andreas <andreas@korzinowski.de>
2017-05-17 00:31:02 +0200

    Annotation für die Datenbank-Joins

---
+++ jsonfile-db/src/io/jsondb/annotation/DBRef.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.jsondb.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Andreas
+ */
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ FIELD })
+public @interface DBRef {
+    String destcollection();
+    String destfieldname();
+}
